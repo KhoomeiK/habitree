2e0384b673dcab5ef9d47e5b9280d2bded56c233
Rohan Pandey <rohan.pandey@gmail.com>
2018-04-24 19:14:12 -0700

    express

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,36 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-// catch 404 and forward to error handler
+app.get('/fire', (req, res) => {
+    res.render('fire');
+});
+
+// app.get('/public/js/main.js', (req, res) => {
+//     res.sendFile('/Users/rohan/Documents/Webwork/habitree/public/js/main.js');
+// });
+
+//catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
