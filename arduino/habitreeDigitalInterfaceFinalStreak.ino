508ed6c141443faf0986090b155bce11b2c8b273
andrewkatchour <38053964+andrewkatchour@users.noreply.github.com>
2018-06-02 23:55:34 -0700

    Add files via upload

---
+++ arduino/habitreeDigitalInterfaceFinalStreak.ino
@@ -0,0 +1,347 @@
+/*
+ * 16x16 Dot Matrix and 16x2 LCD - Change Emotion
+ * 
+ * Written by Andrew Katchour and Jared Ho
+ * 
+ * Starter code for matrix from https://www.hackster.io/Treebug842/16x16-led-matrix-display-012b1e
+ * 
+ * Funtion of the code is to change the emotion displayed on the 16x16 dot matrix along with a corresponding message
+ * written on the 16x2 LCD screen by recieving an Anolog input from another arduino.
+*/
+
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+
+const int rs = 12, en = 11, d4 = 10, d5 = 13, d6 = 14, d7 = 15;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+#define LEDARRAY_D 2
+#define LEDARRAY_C 3
+#define LEDARRAY_B 4
+#define LEDARRAY_A 5
+#define LEDARRAY_G 6
+#define LEDARRAY_DI 7
+#define LEDARRAY_CLK 8
+#define LEDARRAY_LAT 9
+
+#define ANALOG_IN 17
+#define ANALOG_STREAK 19
+
+static unsigned char currentScene = 0;
+static unsigned char newScene = 0;
+static boolean isPressed = false;
+
+
+
+unsigned char Display_Buffer[2];
+
+unsigned int Word1[32];
+
+
+unsigned char  Happy[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+unsigned char  Meh[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+unsigned char  Sad[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+unsigned char  Angry[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1},
+    {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+unsigned char  Dead[16][16] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+    {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+};
+
+void SceneToWord(unsigned char Scene[16][16]){
+  int i, k, key = 0;
+  unsigned int value;
+  for(i = 0; i < 16; i++){
+    for(k = 0; k < 16; k++){
+      if(i < 8){
+        value = Scene[i][k] << (7 - i);
+        Word1[15 - k] += value;
+      } else {
+        value = Scene[i][k] << (15 - i);
+        Word1[31 - k] += value;
+      }
+    }
+  }
+}
+
+ 
+void setup() {
+  pinMode(LEDARRAY_D, OUTPUT); 
+  pinMode(LEDARRAY_C, OUTPUT);
+  pinMode(LEDARRAY_B, OUTPUT);
+  pinMode(LEDARRAY_A, OUTPUT);
+  pinMode(LEDARRAY_G, OUTPUT);
+  pinMode(LEDARRAY_DI, OUTPUT);
+  pinMode(LEDARRAY_CLK, OUTPUT);
+  pinMode(LEDARRAY_LAT, OUTPUT);
+
+  pinMode(ANALOG_IN, INPUT);
+  pinMode(ANALOG_STREAK, INPUT);
+  
+  Serial.begin(9600);
+}
+
+
+
+//*******************************************************
+void Display(unsigned int dat[])  {
+  unsigned char i;
+
+  for( i = 0 ; i < 16 ; i++ )
+  {
+    digitalWrite(LEDARRAY_G, HIGH);   
+    
+    Display_Buffer[0] = dat[i];   
+    Display_Buffer[1] = dat[i+16];
+
+    Send(Display_Buffer[1]);
+    Send(Display_Buffer[0]);
+
+    digitalWrite(LEDARRAY_LAT, HIGH);          
+    delayMicroseconds(1);
+  
+    digitalWrite(LEDARRAY_LAT, LOW);
+    delayMicroseconds(1);
+
+    Scan_Line(i);             
+
+    digitalWrite(LEDARRAY_G, LOW);
+    
+    delayMicroseconds(100);   
+  } 
+}
+
+//*******************************************************
+void Scan_Line( unsigned int m) { 
+  switch(m)
+  {
+    case 0:     
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);          
+      break;
+    case 1:         
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);     
+      break;
+    case 2:         
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);     
+      break;
+    case 3:         
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);    
+      break;
+    case 4:
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);     
+      break;
+    case 5:
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);    
+      break;
+    case 6:
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);    
+      break;
+    case 7:
+      digitalWrite(LEDARRAY_D, LOW);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);     
+      break;
+    case 8:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);     
+      break;
+    case 9:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);    
+      break;  
+    case 10:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);    
+      break;
+    case 11:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, LOW);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);     
+      break;
+    case 12:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, LOW);    
+      break;
+    case 13:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, LOW);digitalWrite(LEDARRAY_A, HIGH);     
+      break;
+    case 14:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, LOW);     
+      break;
+    case 15:
+      digitalWrite(LEDARRAY_D, HIGH);digitalWrite(LEDARRAY_C, HIGH);digitalWrite(LEDARRAY_B, HIGH);digitalWrite(LEDARRAY_A, HIGH);    
+      break;
+    default : break;  
+  }
+}
+
+//*******************************************************
+
+void Send( unsigned int dat) {
+  unsigned char i;
+  digitalWrite(LEDARRAY_CLK, LOW);
+  delayMicroseconds(1);;  
+  digitalWrite(LEDARRAY_LAT, LOW);
+  delayMicroseconds(1);;
+
+  for( i = 0 ; i < 8 ; i++ )
+  {
+    if( dat&0x01 )
+    {
+      digitalWrite(LEDARRAY_DI, HIGH);  
+    }
+    else
+    {
+      digitalWrite(LEDARRAY_DI, LOW);
+    }
+
+    delayMicroseconds(1);
+    digitalWrite(LEDARRAY_CLK, HIGH);         
+    delayMicroseconds(1);
+    digitalWrite(LEDARRAY_CLK, LOW);
+    delayMicroseconds(1);   
+    dat >>= 1;
+  }   
+}
+    
+void loop() {
+  Display(Word1);
+  clearWord();
+  
+ int val = pulseIn(ANALOG_IN, HIGH);
+ Serial.println(val);
+
+ if (val >= 191)
+  newScene = 1;
+ else if (val < 191 && val >= 128)
+  newScene = 2;
+ else if (val < 128 && val >= 64)
+  newScene = 3;
+ else if (val < 64 && val > 0)
+  newScene = 4;
+ else
+  newScene = 5;
+
+  unsigned char scene = newScene;
+  if (scene == 1) 
+    SceneToWord(Happy);
+  else if(scene == 2)
+    SceneToWord(Meh);
+  else if (scene == 3)
+    SceneToWord(Sad);
+  else if (scene == 4)
+    SceneToWord(Angry);
+  else
+    SceneToWord(Dead);
+  
+//  if (currentScene == scene)
+//      return;
+  
+  currentScene = scene;
+  lcd.display(); //turn on display
+  lcd.begin(16,2);
+  
+  
+  int streak = pulseIn(ANALOG_STREAK, HIGH) - 3;
+  Serial.println(streak);
+  Serial.println();
+  //delay(100);
+  
+  // unsigned int streak = 20;
+  
+  lcd.print("Your streak is: ");
+  if ((streak >= 0) && (streak < 255)) {
+    lcd.setCursor(6,1);
+    lcd.print(streak);
+  }
+  else if (streak = 255){
+    lcd.setCursor(4,1);
+    lcd.print("MAXIMUM");
+}
+
+ // delay(100);
+}
+
+
+void clearWord(){
+  for(int i = 0; i < 32; i++)
+    Word1[i] = 0;
+}
---
+++ arduino/habitreeServoFinal2.ino
@@ -0,0 +1,34 @@
+/*
+ * Created by Andrew Katchour with some code from https://www.arduino.cc/en/Tutorial/Sweep
+ * The function of this code is to turn on a servo when needed.
+ */
+#include <Servo.h>
+
+#define STARTSERVO 7
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(180);
+  delay(15);
+  pinMode(STARTSERVO, INPUT);
+}
+
+
+void loop() {
+   if (digitalRead(STARTSERVO) == HIGH)
+   {
+    myservo.write(0);
+    delay(15);
+   }
+   else
+   {
+    myservo.write(180);
+    delay(15);
+   }
+
+}
